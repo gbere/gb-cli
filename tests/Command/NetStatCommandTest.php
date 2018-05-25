e08cabb5b89908bcba4fd1e02aff5867ed6a63eb
Gerard Berengué <gerard@bere.cat>
2018-05-25 19:09:55 +0200

    net:stat command test

---
+++ tests/Command/NetStatCommandTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Command;
+
+use App\Command\NetStatCommand;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+
+class NetStatCommandTest extends KernelTestCase
+{
+    public function testExecute()
+    {
+        $kernel = self::bootKernel();
+        $application = new Application($kernel);
+        $application->add(new NetStatCommand());
+
+        $command = $application->find('net:stat');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([
+            'command' => $command->getName(),
+        ]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertContains('COMMAND', $output);
+        $this->assertContains('PID', $output);
+    }
+}
